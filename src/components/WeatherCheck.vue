f506a93beb98cf7fc691fa04e3c82021b4d14714
Mariya Lazarova <mariya@sumdog.com>
2021-02-14 20:12:37 +0000

    create basic app functionality to show the temperature

---
+++ src/components/WeatherCheck.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <h1>Weather App</h1>
+    <button @click="weatherPicker">Check the temperature</button>
+    <div v-show="visible">
+      <p> The temperature in {{ currentWeather.location }} is
+        {{ currentWeather.temperature }}
+      </p>
+      <p v-if="locatedInGlasgow">
+          You should wear a {{ clothesAdvice }}
+      </p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      weatherData: [
+        {
+          "location" : "Edinburgh",
+          "temperature" : 10
+        },
+        {
+          "location" : "Glasgow",
+          "temperature" : 15
+        },
+        {
+          "location" : "Edinburgh",
+          "temperature" : 20
+        },
+        {
+          "location" : "Glasgow",
+          "temperature" : 10
+        },
+        {
+          "location" : "London",
+          "temperature" : 10
+        },
+        {
+          "location" : "Glasgow",
+          "temperature" : 25
+        }
+      ],
+      currentWeather: '',
+      visible: false
+    }
+  },
+
+  methods: {
+    weatherPicker(){
+      let weatherEntry = Math.floor(Math.random() * this.weatherData.length);
+      this.currentWeather = this.weatherData[weatherEntry];
+      this.visible = true;
+    }
+  },
+
+  computed: {
+    locatedInGlasgow(){
+      return this.currentWeather.location === 'Glasgow';
+    },
+
+    clothesAdvice(){
+      return this.currentWeather.temperature >= 15 ? 't-shirt' :  'jumper';
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
